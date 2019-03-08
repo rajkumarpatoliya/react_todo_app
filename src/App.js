68be0f81e3ea14756bea40ca11ddfcd83c430728
Rajkumar Patoliya <patoliyaraj@gmail.com>
2019-03-08 11:29:37 +0530

    updated the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
+import Todos  from "./components/Todos";
 
 class App extends Component {
+  state = {
+    todos : [
+      {
+        id: 1,
+        title: "take out the trash.",
+        completed: false
+      },
+      {
+        id: 2,
+        title: "Cook breakfast.",
+        completed: false
+      },
+      {
+        id: 3,
+        title: "Dinner with Girlfriend.",
+        completed: false
+      },
+    ]
+  }
   render() {
+    console.log(this.state.todos);
     return (
       <div className="App">
-        <h1>App</h1>
+        <Todos todos={this.state.todos} />
       </div>
     );
   }
